42db9176cf7cb7e993373d980ae4bba42debe07b
stilist1 <125565957+stilist1@users.noreply.github.com>
2024-12-24 04:24:54 +0100

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -3,13 +3,13 @@ import string
 
 
 def generatePassword(length):
-    # Проверка на допустимую длину
+    # Checking len
     if length < 1 or length > 25:
         raise ValueError("Password length must be between 1 and 25.")
 
-    # Генерация пароля
+    # Gen. pass
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
 
-    # Разделение пароля на строки по 15 символов
+    # Split strings 15
     return '\n'.join(password[i:i + 15] for i in range(0, len(password), 15))
