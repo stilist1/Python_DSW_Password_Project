65df9b6cc45fac0530b26715922a6c137a25722a
stilist1 <125565957+stilist1@users.noreply.github.com>
2025-01-13 11:27:47 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,27 +1,22 @@
 import unittest
-from funcs import generatePassword  # Предполагается, что функция generatePassword импортируется из funcs.py
+from funcs import generatePassword 
 
 
 class TestPasswordGenerator(unittest.TestCase):
 
-    # Тест на генерацию пароля с корректной длиной
     def test_generate_valid_password(self):
         length = 10
-        password = generatePassword(length)  # Вызываем функцию для генерации пароля
-
-        # Проверяем, что длина пароля соответствует введенной
+        password = generatePassword(length)  
         self.assertEqual(len(password), length)
 
-    # Тест на генерацию пароля с некорректной длиной (меньше 1)
     def test_generate_invalid_length_low(self):
         length = 0
-        with self.assertRaises(ValueError):  # Ожидаем, что будет выброшено исключение
+        with self.assertRaises(ValueError): 
             generatePassword(length)
 
-    # Тест на генерацию пароля с некорректной длиной (больше 25)
     def test_generate_invalid_length_high(self):
         length = 30
-        with self.assertRaises(ValueError):  # Ожидаем, что будет выброшено исключение
+        with self.assertRaises(ValueError): 
             generatePassword(length)
 
 
